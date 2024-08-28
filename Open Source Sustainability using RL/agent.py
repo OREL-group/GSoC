702c9e2615e444124da32da40442946bbea7b469
ShubhamS2003 <sandhyayamlur1@gmail.com>
2024-08-28 23:24:25 +0530

    Added some RL logic

--- Open Source Sustainability using RL/agent.py
+++ Open Source Sustainability using RL/agent.py
@@ -13,6 +13,7 @@ class IssueAgent(Agent):
         self.creation_time = self.model.schedule.time  # Time when the issue is created
         self.marked_time = None  # Time when the issue is marked for review
         self.review_time = None  # Time when the issue is reviewed
+        self.c
 
     def step(self):
         # if self.marked_for_review and self.pr_accepted:
@@ -26,46 +27,40 @@ class ContributorAgent(Agent):
         self.skill_level = skill_level  # Skill levels: Beginner, Intermediate, Advanced
 
     def step(self):
-        self.move()
-        self.check_issue()
-
-    def move(self):
-        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
-
-    def check_issue(self):
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        for agent in cellmates:
-            if isinstance(agent, IssueAgent) and agent.marked_for_review == False:
-                agent.marked_for_review = True
-                agent.code_efficiency = random.randint(1, 100)  # Assign efficiency when marked for review
+        action_tuple = self.model.action_dict[self.unique_id]
+        issue_pos = self.neighborhood[action_tuple[0]]
+        for agent in self.model.grid.get_cell_list_contents(self.neighborhood):
+            if isinstance(agent, IssueAgent) and agent.marked_for_review == False and agent.pos == issue_pos:
+                agent.marked_for_review == True
+                agent.code_efficiency == random.randint(1, 100)
                 agent.marked_time = self.model.schedule.time
-                print(f'{self.skill_level} ContributorAgent {self.unique_id} found {agent.difficulty} IssueAgent {agent.unique_id} at {self.pos} and marked it for review with efficiency {agent.code_efficiency}')
-
+                break
+        self.update_neighbors()
+        # Move based on action
+        if self.model.movement:
+            move(self, self.model.action_dict[self.unique_id][1], self.empty_neighbors)
+        # Update the neighbors for observation space
+        self.update_neighbors()
 class MaintainerAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
     def step(self):
-        self.move()
-        self.check_issue_for_review()
-
-    def move(self):
-        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
-
-    def check_issue_for_review(self):
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        for agent in cellmates:
-            if isinstance(agent, IssueAgent) and agent.marked_for_review and agent.pr_accepted == False:
-                print(f'Maintainer {self.unique_id} found marked {agent.difficulty} IssueAgent {agent.unique_id} at {self.pos} with efficiency {agent.code_efficiency}')
-                if agent.code_efficiency > 50:
+        action_tuple = self.model.action_dict[self.unique_id]
+        issue_pos = self.neighborhood[action_tuple[0]]
+        for agent in self.model.grid.get_cell_list_contents(self.neighborhood):
+            if isinstance(agent, IssueAgent) and agent.marked_for_review and agent.pr_accepted == False and agent.pos == issue_pos:
+                if agent.code_efficiency>70:
                     agent.pr_accepted = True
                     agent.review_time = self.model.schedule.time
-                    self.model.high_efficiency_issues.append(agent.unique_id)
                 else:
                     agent.marked_for_review = False
+        
+        self.update_neighbors()
+        # Move based on action
+        if self.model.movement:
+            move(self, self.model.action_dict[self.unique_id][1], self.empty_neighbors)
+        # Update the neighbors for observation space
+        self.update_neighbors()
 
 
