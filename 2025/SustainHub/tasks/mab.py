38f4066a58a4481b3a1c9f088c7a97dff6ad5c3e
vidhirohira <159313286+vidhirohira@users.noreply.github.com>
2025-06-13 01:17:00 +0530

    Update mab.py

--- 2025/SustainHub/tasks/mab.py
+++ 2025/SustainHub/tasks/mab.py
@@ -11,14 +11,14 @@ class MABAllocator:
         """Select an agent using Thompson Sampling (a Bayesian MAB approach)"""
         task_type = task.task_type
         
-        # If we're exploring (based on exploration_factor), choose randomly
+        # If we're exploring, choose randomly
         if np.random.random() < self.exploration_factor:
             available_agents = [a for a in self.agents if a.task_load < a.max_load]
             if available_agents:
                 return np.random.choice(available_agents)
             return None
         
-        # Otherwise, use Thompson Sampling to select the best agent
+        # Otherwise, Thompson Sampling
         max_sample = -1
         best_agent = None
         
@@ -31,7 +31,7 @@ class MABAllocator:
             successes = agent.success_counts[task_idx]
             failures = agent.total_counts[task_idx] - successes
             
-            #  (Bayesian approach)
+            #Bayesian approach
             sample = beta.rvs(successes + 1, failures + 1)
             
             if sample > max_sample:
